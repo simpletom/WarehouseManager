3780b7eed6763c3d092309297a77b674f09e1974
Thomas Bednorz <tbednorz@gmx.de>
2021-02-23 15:58:50 +0100

    Added CustomTimestamp model

--- src/com/whm/models/CustomTimestamp.java
+++ src/com/whm/models/CustomTimestamp.java
@@ -1,2 +1,24 @@
-package com.whm.models;public class CustomTimeStamp {
+package com.whm.models;
+
+public class CustomTimestamp {
+    private int day;
+    private int month;
+    private int year;
+
+    private int hour;
+    private int minute;
+    private int second;
+
+    public CustomTimestamp(int day, int month, int year, int hour, int minute, int second) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+
+    public String getTimeStamp() {
+        return this.day + " " + this.month + " " + this.year + " " + this.hour + ":" + this.minute + ":" + this.second;
+    }
 }
