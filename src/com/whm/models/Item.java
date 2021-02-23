9b0c7db534db782b0bb30e231d3417e817408788
Thomas Bednorz <tbednorz@gmx.de>
2021-02-23 15:58:00 +0100

    Added item model

--- src/com/whm/models/Item.java
+++ src/com/whm/models/Item.java
@@ -1,9 +1,23 @@
 package com.whm.models;
 
-public class Product {
-    private int ID;
+public class Item {
     private String name;
     private int basePrice;
+    private int quantity;
+
+    public Item(String name, int basePrice, int quantity) {
+        this.name = name;
+        this.basePrice = basePrice;
+        this.quantity = quantity;
+
+    }
+    public int getQuantity() {
+        return this.quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 
     public void setName(String name) {
         this.name = name;
@@ -13,15 +27,11 @@ public class Product {
         this.basePrice = basePrice;
     }
 
-    public int getID() {
-        return ID;
-    }
-
     public String getName() {
-        return name;
+        return this.name;
     }
 
     public int getBasePrice() {
-        return basePrice;
+        return this.basePrice;
     }
 }
