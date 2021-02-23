610906a3ebf1a10cb6b17a615c8c918af8a16b40
Thomas Bednorz <tbednorz@gmx.de>
2021-02-23 15:59:56 +0100

    Added WarehouseNotFoundException

--- src/com/whm/exceptions/WarehouseNotFoundException.java
+++ src/com/whm/exceptions/WarehouseNotFoundException.java
@@ -1,2 +1,7 @@
-package com.whm.exceptions;public class WarehouseNotFoundException {
+package com.whm.exceptions;
+
+public class WarehouseNotFoundException extends Exception {
+    public WarehouseNotFoundException(String error) {
+        super(error);
+    }
 }
